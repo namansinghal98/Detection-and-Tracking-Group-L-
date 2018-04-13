d031c6cc4be613725a81e8e0aff7d147f7520678
namansinghal98 <namansinghal198@gmail.com>
2018-04-13 13:25:33 +0530

    Delete function1.m

--- function1.m
+++
@@ -1,6 +0,0 @@
-% FILE: function1.m
-% Use this function as integrand to evaluate the probability of detection
-% It is called from pd1graph which graphs POD vs SNR for fixed Prob of False Alarm
-function f = function1(v)
-global temp
-f = v .* exp(-(v.^2 + temp.^2)/2) .* besseli(0, temp.*v);
