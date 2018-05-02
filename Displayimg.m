7a88c4a5b615b93e0e12ad91ddb5a9e28dd8fe8e
ayrustogaru <35329371+ayrustogaru@users.noreply.github.com>
2018-05-02 09:01:47 +0530

    Add files via upload

--- Displayimg.m
+++ Displayimg.m
@@ -1,4 +1,14 @@
 function Displayimg(file_path)
+% This function generates the surface plots of the target at various
+% instance of the traversal. The absolute file path of the folder in which
+% the images are generated is to be passed to this function. The file path
+% sent as an input should be a string and engulfed in quotes. The surface
+% plots generated he surface diagram consists of peaks which represents the target. 
+% The output is three mesh diagrams, one, which shows the complete traversal 
+% of the peak i.e. target; two, which plots the position of target in every 
+% fourth image when there are large number of images and the last one, 
+% shows the start and end points of the target.
+%% Error Handling %%
 fileFolder = fullfile(file_path);
 dirOutput = dir(fullfile(fileFolder,'test_*.png'));
 fileNames = {dirOutput.name}';
@@ -7,6 +17,7 @@ if(isempty(fileNames))
 end
 global num_of_img;
 global bound;
+%% Surface plot of every Image combined %%
 figure; hold on
 [X,Y] = meshgrid(-1*bound:1:bound);
 for i = 0:+1:num_of_img-1
@@ -14,13 +25,13 @@ for i = 0:+1:num_of_img-1
 
 surf(X,Y,k);
 colormap default;
-colorbar;
+colorbar; % colorbar for the surface plot
 end
 L = imread(fullfile(file_path,strcat('test_',int2str(num_of_img-1),'.png')));
 surf(X,Y,L);
-view(17,20)
+view(17,20) % The view angle of the figure generated interms of elevation and azimuth
 hold off
-
+%% Surface plot of the start and end points %%
 figure; hold on
 [X,Y] = meshgrid(-1*bound:1:bound);
 for i = 0
@@ -34,7 +45,7 @@ L = imread(fullfile(file_path,strcat('test_',int2str(num_of_img-1),'.png')));
 surf(X,Y,L);
 view(17,20);
 hold off
-
+%% Surface plot of every 5th image %%
 figure;
 hold on
 if(num_of_img>5)
