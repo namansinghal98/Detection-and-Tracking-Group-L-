49d86b9c14f347dfa5a4f6cfec379c37693de01a
ayrustogaru <35329371+ayrustogaru@users.noreply.github.com>
2018-05-02 07:59:22 +0530

    Update img_coll.m

--- img_coll.m
+++ img_coll.m
@@ -1,10 +1,12 @@
 function img_coll(file_path)
 %Montage of images
-%This function uses the 
+%This function uses the absolute file path of the folder in which the files are located to create a rectangular montage of all the images 
+%of the target generated using the img_gen function. The montage will contain 10 pictures in each row, so as to ensure that the collage
+%doesn't reduce to a small size.
 fileFolder = fullfile(file_path);
-dirOutput = dir(fullfile(fileFolder,'test_*.png'));
+dirOutput = dir(fullfile(fileFolder,'test_*.png')); %All the png files starting with 'test_' are taken in to create a matrix of the filenames
 fileNames = {dirOutput.name}';
-fileNames = natsort(fileNames);
+fileNames = natsort(fileNames); %The natsort function is used to sort the filenames of the images the natural order.
 num = numel(fileNames);
 rows = floor(num/10);
 if(rows == num/10)
