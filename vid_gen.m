10b5dd1cf017f009de43a8776d5c7fcc45578e75
ayrustogaru <35329371+ayrustogaru@users.noreply.github.com>
2018-05-02 08:49:35 +0530

    Update vid_gen.m

--- vid_gen.m
+++ vid_gen.m
@@ -1,12 +1,14 @@
 function vid_gen(file_path)
+% This function is the video generator which creates a video name target.avi by combining all the images generated using img_gen function.
+% The frame rate of the video is taken to be 25 frames per second.
 fileFolder = fullfile(file_path);
-dirOutput = dir(fullfile(fileFolder,'test_*.png'));
+dirOutput = dir(fullfile(fileFolder,'test_*.png')); % The png files starting with 'test_' are taken into account
 fileNames = {dirOutput.name}';
-fileNames = natsort(fileNames);
+fileNames = natsort(fileNames);% Natural sorting of the filenames
 outputVideo = VideoWriter(fullfile(file_path, 'target.avi'));
-outputVideo.FrameRate = 25;
+outputVideo.FrameRate = 25;% Frame rate of the video
 open(outputVideo);
-for i = 1:length(fileNames)
+for i = 1:length(fileNames)% Combinig the images
    img = imread(fullfile(file_path,strcat('test_',int2str(i-1),'.png')));
    writeVideo(outputVideo,img)
 end
